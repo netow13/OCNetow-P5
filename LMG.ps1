551dcbf959247c99fd6c4c959a79f80112e50df5
netow13 <58041232+netow13@users.noreply.github.com>
2020-01-28 21:54:25 +0100

    Add files via upload

---
+++ LMG.ps1
@@ -0,0 +1,22 @@
+﻿#Script exportation des membres d'un groupe dans un fichier texte
+Import-Module activedirectory
+
+#Variable
+
+$groupe = Read-Host "Entrez le nom du groupe dont vous souhaitez la liste des membres"
+
+#Teste de la présence du groupe dans AD
+
+try
+{
+    $ADgroupe = Get-ADGroup -Identity $groupe -ErrorAction Stop
+
+#Exportation des membres d'un groupe dans un fichier texte si ce dernier existe
+    
+    $liste = get-ADGroupMember -Identity $groupe | Get-ADUser -Properties Mail,OfficePhone,MobilePhone | select Name,Mail,OfficePhone,MobilePhone | out-file C:\Users\Administrateur.WIN-LPRU2UTKF0H\Desktop\Groupe_concerné_$groupe.txt
+   
+}
+catch
+{
+    "Groupe non trouvé"
+}
