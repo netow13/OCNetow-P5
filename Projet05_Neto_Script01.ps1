21343b8453a8345340108c41bb12bb9997be9816
netow13 <58041232+netow13@users.noreply.github.com>
2020-01-28 21:04:23 +0100

    Update and rename TestUser.ps1 to Projet05_Neto_Script01.ps1

--- Projet05_Neto_Script01.ps1
+++ Projet05_Neto_Script01.ps1
@@ -1,4 +1,7 @@
-﻿# Importe le module active directory pour utiliser les commandlets AD
+# Script création d'un utilisateur dans l'AD à partir d'un fichier CSV, avec l'ajout de ce dernier dans un groupe
+# et la création d'un dossier partagé
+
+# Importe le module active directory pour utiliser les commandlets AD
 Import-Module activedirectory
   
 # Stocke les données du fichier CSV dans la variable $ADUsers
@@ -7,34 +10,34 @@ $ADUsers = Import-csv C:\Users\Administrateur.WIN-LPRU2UTKF0H\Desktop\test2.csv 
 # Boucle pour chaque rangée contenant les details d'un utilisateur dans un fichier CSV 
 foreach ($User in $ADUsers)
 {
-	# Lis les données utilisateur dans chaque champs de chaque rangée et assigne la donnée à une variable ci-dessous	
+# Lis les données utilisateur dans chaque champs de chaque rangée et assigne la donnée à une variable ci-dessous	
 	
-	$Username 	= $User.LoginNT
-	$Password 	= $User.PswdNT
-	$Firstname 	= $User.Prenom
-	$Lastname 	= $User.Nom
+	$Username = $User.LoginNT
+	$Password = $User.PswdNT
+	$Firstname = $User.Prenom
+	$Lastname = $User.Nom
 	$Group = $User.Departement
-    $OfficePhone = $User.Tel2
-    $MobilePhone = $User.Mobile
-    $Email = $User.email
+	$OfficePhone = $User.Tel2
+    	$MobilePhone = $User.Mobile
+    	$Email = $User.email
     
 
 
-	# Vérification de l'existance de l'utilisateur dans l'AD
+# Vérification de l'existance de l'utilisateur dans l'AD
 	if (Get-ADUser -F {SamAccountName -eq $Username})
 	{
-		 # Si l'utilisateur existe déjà, message d'erreur
+# Si l'utilisateur existe déjà, message d'erreur
 		 Write-Warning "Un compte utilisateur avec pour login $Username existe déjà dans l'Active Directory."
 	}
 	else
 	{
-		# L'utilisateur n'existe pas donc le script cré un nouveau compte utilisateur
+# L'utilisateur n'existe pas donc le script cré un nouveau compte utilisateur
 		
 		New-ADUser -SamAccountName $Username -UserPrincipalName "$Username@acme.sp" -Name "$Firstname $Lastname" -GivenName $Firstname -Surname $Lastname -OfficePhone $OfficePhone -MobilePhone $MobilePhone -EmailAddress $Email -Enabled $True -DisplayName "$Lastname, $Firstname" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -ChangePasswordAtLogon $True
         
-        # Ajoute l'utilisateur à son groupe
+# Ajoute l'utilisateur à son groupe
 
-        Add-ADGroupMember -Identity $Group -Members $Username             
+        	Add-ADGroupMember -Identity $Group -Members $Username             
 
 	}
 }
