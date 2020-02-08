044e468c7c308970d2b799a887716810702b048f
netow13 <58041232+netow13@users.noreply.github.com>
2020-02-08 16:55:47 +0100

    Update Projet05_Neto_Script04.ps1

--- Projet05_Neto_Script04.ps1
+++ Projet05_Neto_Script04.ps1
@@ -1,4 +1,5 @@
+$NomMachine = [system.environment]::MachineName
 $DirToBackup = "C:\Users"
 $Backup = "\\SRVADPAR01\Sensible"
-Remove-Item $Backup -Recurse -Force
-Copy-Item -Path $DirToBackup\* -Destination $Backup -Recurse -Force
+Remove-Item $Backup\$NomMachine -Recurse -Force
+Copy-Item -Path $DirToBackup\* -Destination $Backup\$NomMachine -Recurse -Force
