b5f21c1fca7240dbc2ce40504d28cf8553910e30
netow13 <58041232+netow13@users.noreply.github.com>
2020-02-08 17:06:54 +0100

    Update Projet05_Neto_Script04.ps1

--- Projet05_Neto_Script04.ps1
+++ Projet05_Neto_Script04.ps1
@@ -1,11 +1,11 @@
 # Script de sauvegarde de chaque poste client dans un dossier c:\SAV\ dédié sur le serveur
 
-$NomMachine = [system.environment]::MachineName
 $DirToBackup = "C:\Users"
-$Backup = "\\SRVADPAR01\Sensible"
+$Backup = "\\SRVADPAR01\Sensible\[sytem.environment]::MachineName"
 
+if(Test-Path $Backup
 # Suppression de la précédente sauvegarde
-Remove-Item $Backup\$NomMachine -Recurse -Force
+Remove-Item $Backup\$NomMachine\* -Recurse -Force
 
 # Nouvelle sauvegarde
 Copy-Item -Path $DirToBackup\* -Destination $Backup\$NomMachine -Recurse -Force
