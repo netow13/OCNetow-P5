9f1bc556078aab7c95eed7e78c462a2df5f9f0e1
netow13 <58041232+netow13@users.noreply.github.com>
2020-03-11 10:58:53 +0100

    Update Projet05_Neto_Script03.ps1
    Différentes corrections.

--- Projet05_Neto_Script03.ps1
+++ Projet05_Neto_Script03.ps1
@@ -1,23 +1,25 @@
 # Script exportation liste des groupes dont un utilisateur est membre dans un fichier texte via ligne de commande
+
 param(
     [string] $username
     )
 
 Import-Module activedirectory
 
-$UserN = [Environment]::UserName
 
 # Teste de la présence de l'utilisateur dans AD
 try
 {    
-    $ADUser = Get-ADUser -Identity $username -ErrorAction Stop
+    Get-ADUser -Identity $username -ErrorAction Stop
     
 #Exportation des groupes auquels appartient l'utilisateur dans un fichier texte
 
-    $liste = Get-ADPrincipalGroupMembership -Identity $username | get-adgroup -properties name | select name | out-file C:\Users\$UserN\Desktop\Projet05_Neto_AD03.txt
+    Get-ADPrincipalGroupMembership -Identity $username | Get-ADGroup -properties name | select name | Out-File C:\Temp\Projet05_Neto_AD03.txt
+
+    Write-Host "Exportation effectuée! Retrouvez le fichier texte AD03 dans le dossier C:\Temp."
     
 }
 catch
 {
-    "Membre non trouvé"
+    "Une erreur est survenue"
 }
