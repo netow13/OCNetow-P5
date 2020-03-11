73b56a578cf9f58f6cf42aeb6ded4223f53a5840
netow13 <58041232+netow13@users.noreply.github.com>
2020-03-11 10:59:55 +0100

    Update Projet05_Neto_Script04.ps1
    Debug et corrections.

--- Projet05_Neto_Script04.ps1
+++ Projet05_Neto_Script04.ps1
@@ -1,22 +1,24 @@
-# Script de sauvegarde de chaque poste client dans un dossier c:\SAV\ dédié sur le serveur
+# Script de sauvegarde de chaque poste client dans un dossier C:\SAV\ dédié sur le serveur
 
-$NomMachine = "[system.environment]::MachineName"
+$NomMachine = [system.environment]::MachineName
 $DirToBackup = "C:\Users"
-$Backup = "\\SRVADPAR01\sav"
+$Backup = "\\SRVADPAR01\sav\$NomMachine"
+$exclude = @("ntuser*", "AppData")
 
 # Teste de l'existance d'un dossier de sauvegarde selon la machine
-if(Test-Path $Backup\$NomMachine -eq True)
+if(Test-Path $Backup\$NomMachine)
 {  
 # Suppression de la précédente sauvegarde
   Remove-Item $Backup\$NomMachine\* -Recurse -Force
 
-# Nouvelle sauvegarde
-  Copy-Item -Path $DirToBackup\* -Destination $Backup\$NomMachine -Recurse -Force
-}
+# Nouvelle sauvegarde du dossier C:\User en excluant les fichiers ntuser et dossiers AppData
+  Get-ChildItem $DirToBackup -Recurse -Exclude $exclude | Copy-Item -Destination {join-path $Backup $_.FullName.Substring($DirToBackup.Length)}
+  }
 else
 {
 # Création d'un dossier de sauvegarde pour la nouvelle machine
   New-Item -Path $Backup -Name $NomMachine -ItemType directory
-# Première sauvegarde  
-  Copy-Item -Path $DirToBackup\* -Destination $Backup\$NomMachine -Recurse -Force
+
+# Première sauvegarde du dossier C:\User en excluant les fichiers ntuser et dossiers AppData
+  Get-ChildItem $DirToBackup -Recurse -Exclude $exclude | Copy-Item -Destination {join-path $Backup $_.FullName.Substring($DirToBackup.Length)}
 }
